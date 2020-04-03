668c04d7bb69870479f4ee60b955b77fc4fb7941
Alexander <angelez2050@hotmail.com>
2020-04-02 21:24:12 -0500

    Primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,24 @@
+import Server from './clases/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+const server = new Server();
+
+// BodyParser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+
+server.app.use(bodyParser.json());
+
+// CORS
+server.app.use(cors({ origin: true, credentials: true }));
+
+
+//RUTAS DE SERVICIOS
+server.app.use('/', router)
+
+
+server.start(() => {
+    console.log(`Servidor corriendo en el puerto ${server.port}`);
+});
